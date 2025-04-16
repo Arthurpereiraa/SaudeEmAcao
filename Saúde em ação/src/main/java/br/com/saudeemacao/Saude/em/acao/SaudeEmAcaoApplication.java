37a0cc3da5d8bd2dbd9b8624c71a9f45c12c672e
arthurpereiraa <arthurpsf8@gmail.com>
2025-04-16 11:13:49 -0300

    Primeiro commit do projeto Academia

---
+++ Saúde em ação/src/main/java/br/com/saudeemacao/Saude/em/acao/SaudeEmAcaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.saudeemacao.Saude.em.acao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeEmAcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeEmAcaoApplication.class, args);
+	}
+
+}
---
+++ Saúde em ação/src/test/java/br/com/saudeemacao/Saude/em/acao/SaudeEmAcaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saudeemacao.Saude.em.acao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeEmAcaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
